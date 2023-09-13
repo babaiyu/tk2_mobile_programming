28b46a68e06da97028735a6989bf7059ca95d3bc
babaiyu <bayuputra9a@gmail.com>
2023-09-13 18:28:59 +0700

    First commit

--- App.js
+++ App.js
@@ -1,20 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
+// Import all screen here...
+import LoginScreen from "./src/screens/LoginScreen";
+import CatalogScreen from "./src/screens/CatalogScreen";
+
+const Stack = createNativeStackNavigator();
+
+// Main program
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="LOGIN_SCREEN">
+        <Stack.Screen name="LOGIN_SCREEN" component={LoginScreen} />
+        <Stack.Screen name="CATALOG_SCREEN" component={CatalogScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/screens/CatalogScreen/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { SafeAreaView, Text } from "react-native";
+
+export default function CatalogScreen() {
+  return (
+    <SafeAreaView>
+      <Text>Catalog Screen</Text>
+    </SafeAreaView>
+  );
+}
